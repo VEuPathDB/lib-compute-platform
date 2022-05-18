1b75685534155218f90a1ebb1269270aea4a70c6
Elizabeth <foxcapade@gmail.com>
2022-05-18 15:03:19 -0400

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,29 @@ group = "org.veupathdb.lib"
 version = "1.0-SNAPSHOT"
 
 repositories {
+  mavenLocal()
   mavenCentral()
+  maven {
+    name = "GitHubPackages"
+    url  = uri("https://maven.pkg.github.com/veupathdb/maven-packages")
+    credentials {
+      username = if (extra.has("gpr.user")) extra["gpr.user"] as String? else System.getenv("GITHUB_USERNAME")
+      password = if (extra.has("gpr.key")) extra["gpr.key"] as String? else System.getenv("GITHUB_TOKEN")
+    }
+  }
 }
 
 dependencies {
+  // Logging
+  implementation("org.slf4j:slf4j-api:1.7.36")
+  implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+  implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
+
+  // S3
+  api("org.veupathdb.lib.s3:s34k:0.4.0")
+  api("org.veupathdb.lib.s3:s34k-minio:0.2.0+s34k-0.4.0")
+  implementation("io.minio:minio:8.3.9")
+
   testImplementation(kotlin("test"))
 }
 
