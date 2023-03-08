be1a140b5936326943863d275b5474b55a0c7a63
Elizabeth <foxcapade@gmail.com>
2023-03-08 09:30:00 -0500

    update docs

--- lib/src/main/kotlin/org/veupathdb/lib/compute/platform/model/JobReference.kt
+++ lib/src/main/kotlin/org/veupathdb/lib/compute/platform/model/JobReference.kt
@@ -2,4 +2,16 @@ package org.veupathdb.lib.compute.platform.model
 
 import org.veupathdb.lib.hash_id.HashID
 
-data class JobReference(val jobID: HashID, val owned: Boolean)
+/**
+ * Reference to a job with a flag that indicates whether the current platform
+ * instance owns the job.
+ *
+ * @since 1.4.0
+ *
+ * @constructor Constructs a new JobReference instance.
+ *
+ * @param jobID ID of the job this reference is referencing.
+ *
+ * @param owned Whether the current platform instance owns the job.
+ */
+data class JobReference internal constructor(val jobID: HashID, val owned: Boolean)
